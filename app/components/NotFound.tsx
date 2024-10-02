18182998b318558a1e9f1352b180996f60aa84f2
Nikita <github@nikiv.dev>
2024-10-02 13:05:42 +0300

    .

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -7,17 +7,11 @@ export function NotFound({ children }: { children?: any }) {
         {children || <p>The page you are looking for does not exist.</p>}
       </div>
       <p className="flex items-center gap-2 flex-wrap">
-        <button
-          onClick={() => window.history.back()}
-          className="bg-emerald-500 text-white px-2 py-1 rounded uppercase font-black text-sm"
-        >
-          Go back
-        </button>
         <Link
           to="/"
           className="bg-cyan-600 text-white px-2 py-1 rounded uppercase font-black text-sm"
         >
-          Start Over
+          Go Home
         </Link>
       </p>
     </div>
